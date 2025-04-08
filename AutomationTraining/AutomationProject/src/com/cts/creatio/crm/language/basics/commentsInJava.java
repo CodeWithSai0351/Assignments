573cce4c8dca093626576f7c54b96f5e643c26ff
Somisetti Sai Ram Krishna <somisettisairamkrishna@Somisettis-MacBook-Air.local>
2025-04-08 23:31:34 +0530

    Testcommit

---
+++ AutomationTraining/AutomationProject/src/com/cts/creatio/crm/language/basics/commentsInJava.java
@@ -0,0 +1,24 @@
+package com.cts.creatio.crm.language.basics;
+
+public class commentsInJava {
+
+	/**
+	 * @author somisettisairamkrishna
+	 * @version 0.2
+	 * @param args
+	 * @return no output
+	 * @throws not using proper keywords and main method program wont run
+	 */
+
+	public static void main(String[] args) {
+		// Single line comments
+		System.out.println("Comments in line in print statement");
+
+		/*
+		 * Multiple Line Comments
+		 */
+		System.out.print("Comments in line in second print statement");
+
+	}
+
+}
